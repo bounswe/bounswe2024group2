00a16e48af688a456fef6e3c335e273a41ddf486
yasinbastug <oyba2003@gmail.com>
2024-04-29 20:41:51 +0300

    -profile added

---
+++ project/client/src/pages/Main/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// set profile href to /profile
+import "./Profile.css";
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            {/* Add your profile information here */}
+        </div>
+    );
+};
+
+export default Profile;
