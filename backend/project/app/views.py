b14b1628bb770699653d481046322fd7c629d31d
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2024-04-29 20:14:45 +0300

    fix in import and settings

--- backend/project/app/views.py
+++ backend/project/app/views.py
@@ -7,7 +7,8 @@ from django.conf import settings
 from django.http.response import JsonResponse
 from django.contrib.auth.models import User
 from app.models import Film, Genre, Director, Actor
-from app.serializers import UserSerializer, FilmSerializer, GenreSerializer, DirectorSerializer, ActorSerializer,WikidataQuerySerializer, FilmPatternWithLimitQuerySerializer
+from app.serializers import *
+#from app.serializers import UserSerializer, FilmSerializer, GenreSerializer, DirectorSerializer, ActorSerializer,WikidataQuerySerializer, FilmPatternWithLimitQuerySerializer, MyTokenObtainPairSerializer, LogoutSerializer
 from rest_framework import permissions, status , viewsets, generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
