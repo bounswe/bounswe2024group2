50c5f626760b756ebed56184f7caf169ef0f2a92
rukiyeaslan <19rasln@gmail.com>
2024-04-30 18:15:59 +0300

    add forgot password page for frontend

--- project/client/src/pages/SignUp/ForgotPassword.js
+++ project/client/src/pages/SignUp/ForgotPassword.js
@@ -1,15 +1,41 @@
 import React, { useState } from 'react';
 import './ForgotPassword.css'; 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function Login() {
+function ForgotPassword() {
   const [email, setemail] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Handle the login logic here, typically sending a request to backend
-    console.log('Logging in', { email});
-  };
+    console.log('Receiving email for forgot password', { email});
 
+    fetch('http://localhost:8000/request-reset-email/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email}),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Request email verification is successful');
+          console.log(response.json())
+          toast.success('Request email verification successful! Please verify your email to reset your password.'); 
+          // Redirect to login page
+          window.location.href = '/login';
+        } else {
+          console.log('Sign up failed');
+          toast.error('Sign up failed! Please try again.');
+          // Handle sign up failure here
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle sign up failure here
+      });
+      
   return (
     <div className="forgotPassword-container">
       <div className="forgotPassword-form">
@@ -18,7 +44,7 @@ function Login() {
           <h1>Forgot Password</h1>
         </div>
         <div className = "forgot-password-explanation">
-            <h1>Type your email so that we can send you a verification code to reset your password</h1>
+            <h1>Reset your password</h1>
         </div>
         <form onSubmit={handleSubmit}>
           <input
@@ -34,5 +60,5 @@ function Login() {
     </div>
   );
 }
-
-export default Login;
+}
+export default ForgotPassword;
