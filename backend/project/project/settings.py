21395c41fbca33a705cd23973be4627bef65c32f
rukiyeaslan <19rasln@gmail.com>
2024-05-16 21:50:11 +0300

    update token expiration time

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 }
 
