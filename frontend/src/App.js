37967c11ca517777c868558c335ea9c1fa216676
Mahmut Buğra Mert <bugramert60@gmail.com>
2024-10-17 03:45:23 +0300

    A mock login page is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Login from "./components/Login.js";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Login />
     </div>
   );
 }
