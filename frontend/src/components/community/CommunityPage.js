a3e8dcd758887a112935dc657978efe8cca6f6e9
halil <halilibrahimkasapoglu@gmail.com>
2024-10-19 13:55:07 +0300

    feat: add main page components
    add simple page components for
    + home
    + markets
    + news
    + portfolio
    + community

---
+++ frontend/src/components/community/CommunityPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CommunityPage = () => {
+    return (
+        <div>
+            <h1>Community Page</h1>
+            <p>Community content goes here</p>
+        </div>
+    );
+}
+
+export default CommunityPage;
+
