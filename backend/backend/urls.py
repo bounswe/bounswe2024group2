a263d7e4dd652483690d2c30c6a0cfebbc7ea089
Rukiye Aslan <rukiye.aslan@getmidas.com>
2024-10-19 18:02:52 +0300

    fix: remove unused urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
 from rest_framework import permissions
 from django.shortcuts import render
 
@@ -26,32 +24,10 @@ from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
-
-# This serves static files during development
-
-
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Financial Markets Blog Project",
-#         default_version='v1',
-#         description="API documentation for Cmpe451 Project",
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
-
-
-# def custom_swagger_view(request):
-#     return render(request, 'swagger-ui.html', context={'schema_url': 'openapi-schema'})
-
-# path('swagger/', custom_swagger_view, name='swagger-ui'),
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # path('docs/', custom_swagger_view, name='swagger-ui'),
     path(
         "docs/",
         SpectacularSwaggerView.as_view(
