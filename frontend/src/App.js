93c166004625794a4721a80c45ed69790855f584
halil <halilibrahimkasapoglu@gmail.com>
2024-10-21 23:06:43 +0300

    feat: add post url to routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,6 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import Login from "./components/login/Login.js";
-import Register from "./components/login/Register.js";
 import ForgotPassword from "./components/login/ForgotPassword.js";
 import Dashboard from "./components/Dashboard.js";
 import NewsPage from "./components/news/NewsPage.js";
@@ -15,15 +13,13 @@ import CommunityPage from "./components/community/CommunityPage.js";
 import MarketsPage from "./components/markets/MarketsPage.js";
 import PortfolioPage from "./components/portfolio/PortfolioPage.js";
 import NotFound from "./components/notfound/NotFound.js";
-import { ToastContainer } from "react-toastify";
+import PostView from "./components/community/PostView.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route
             path="/"
@@ -35,12 +31,11 @@ function App() {
             <Route path="news" element={<NewsPage />} />
             <Route path="portfolio" element={<PortfolioPage />} />
 
+            <Route path="/post/:postId" element={<PostView />} />
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
-
-        <ToastContainer />
       </div>
     </Router>
   );
