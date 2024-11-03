86315dff51b5a03ad464b1da3c2999e09f71023e
Rukiye Aslan <rukiye.aslan@getmidas.com>
2024-11-03 17:43:01 +0300

    feat: Add stock manager

--- backend/marketfeed/models.py
+++ backend/marketfeed/models.py
@@ -3,22 +3,40 @@ from onboarding.models import *
 
 # Models for market feed such as post, portfolio, stock, comment
 
+"""-----------------------------------------------------MANAGERS-----------------------------------------------------"""
+
+class StockManager(models.Manager):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        for stock in query_set:
+            stock.price = stock.fetch_current_stock_price()
+        return queryset
+
+
+"""-------------------------------------------------------MODELS-------------------------------------------------------"""
+
 class Currency(models.Model):
     name = models.CharField(max_length=40)
-    code = models.CharField(max_length=5)
+    code = models.CharField(max_length=5, unique=True)
 
 
 class Stock(models.Model):
     name = models.CharField(max_length=250)
-    symbol = models.CharField(max_length=250)
-    price = models.DecimalField(max_digits=19, decimal_places=10)
+    symbol = models.CharField(max_length=250, unique=True)
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+    objects = StockManager()
+
+    @property
+    def fetch_current_stock_price():
+        #TODO: Stock fetching mechanism to be implemented
+        return 10
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Portfolio(models.Model):
@@ -32,16 +50,17 @@ class Portfolio(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    content = models.CharField(max_length=250)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True, auto_now=True)
-    liked_by = models.ManyToManyField(User, verbose_name="list of users who liked the post")
-    tags = models.ManyToManyField(Tag, verbose_name="list of tags added to the post")
-    portfolios = models.ManyToManyField(Portfolio, verbose_name="list of portfolios added to the post")
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name="liked_posts")
+    tags = models.ManyToManyField(Tag, verbose_name="list of tags")
+    portfolios = models.ManyToManyField(Portfolio, verbose_name="list of portfolios")
 
 
 class Comment(models.Model):
     post_id=models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id=models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=250)
