50882eadf37d333d4b96d07a20c8bb480dfcaf45
Rukiye Aslan <rukiye.aslan@getmidas.com>
2024-11-03 17:26:37 +0300

    feat: Add currency

--- backend/marketfeed/models.py
+++ backend/marketfeed/models.py
@@ -3,29 +3,38 @@ from onboarding.models import *
 
 # Models for market feed such as post, portfolio, stock, comment
 
+class Currency(models.Model):
+    name = models.CharField(max_length=40)
+    code = models.CharField(max_length=5)
+
+
 class Stock(models.Model):
     name = models.CharField(max_length=250)
     symbol = models.CharField(max_length=250)
-    price = models.IntegerField
+    price = models.DecimalField(max_digits=19, decimal_places=10)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=40)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
 
 
 class Portfolio(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length = 150)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, auto_now=True)
     stocks = models.ManyToManyField(Stock, verbose_name="list of stocks in the portfolio")
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=250)
-    created_at = models.DateField()
-    updated_at = models.DateField(null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, auto_now=True)
     liked_by = models.ManyToManyField(User, verbose_name="list of users who liked the post")
     tags = models.ManyToManyField(Tag, verbose_name="list of tags added to the post")
     portfolios = models.ManyToManyField(Portfolio, verbose_name="list of portfolios added to the post")
@@ -34,5 +43,5 @@ class Post(models.Model):
 class Comment(models.Model):
     post_id=models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
     content = models.CharField(max_length=250)
-
