c4cc38f9aab43c21331704c387080ff1d900137c
Kamil Deniz Coşkuner <kamil.coskuner@boun.edu.tr>
2024-11-05 15:59:19 +0300

    feat: add empty profile page file

---
+++ frontend/src/components/profile/ProfilePage.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function ProfilePage() {
+    return (
+        <div>
+            Profile
+        </div>
+    );
+};
+
+export default ProfilePage;
