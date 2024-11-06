e404b89b2b238a3e089ee7293974b564cfbf3001
Kamil Deniz Coşkuner <kamil.coskuner@boun.edu.tr>
2024-11-06 15:27:32 +0300

    feat: add profile page header

--- frontend/src/components/profile/ProfilePage.js
+++ frontend/src/components/profile/ProfilePage.js
@@ -1,11 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import "../../styles/Profile.css";
 
-function ProfilePage() {
-    return (
-        <div>
-            Profile
+// Sample data 
+const userProfile = {
+  name: 'Deniz Coşkuner',
+  username: '@danzio',
+  followers: 151,
+  following: 32,
+  posts: 15,
+  portfolios: 1,
+  comments: 12
+};
+
+const ProfilePage = () => {
+  return (
+    <div className="profile-page">
+      <header className="profile-header">
+        <div className="profile-info">
+          <div className="profile-avatar">
+            <span>IMG</span>
+          </div>
+          <div className="profile-details">
+            <h1>{userProfile.name}</h1>
+            <p>{userProfile.username}</p>
+          </div>
         </div>
-    );
+        <div className="profile-stats">
+          <div className="stat-item">
+            <h2>{userProfile.followers}</h2>
+            <p>Followers</p>
+          </div>
+          <div className="stat-item">
+            <h2>{userProfile.following}</h2>
+            <p>Following</p>
+          </div>
+          <div className="stat-item">
+            <h2>{userProfile.posts}</h2>
+            <p>Posts</p>
+          </div>
+          <div className="stat-item">
+            <h2>{userProfile.portfolios}</h2>
+            <p>Portfolios</p>
+          </div>
+          <div className="stat-item">
+            <h2>{userProfile.comments}</h2>
+            <p>Comments</p>
+          </div>
+        </div>
+      </header>
+
+      
+    </div>
+  );
 };
 
 export default ProfilePage;
