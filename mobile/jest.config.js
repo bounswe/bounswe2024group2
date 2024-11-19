6ebe15d40360dbd707abaf150e0f14493e3fc9d3
m-erkam <m.erkamgokcepinar@gmail.com>
2024-11-19 16:53:45 +0300

    feat: create test for login and profile pages Co-authored-by: Furkan Şenkal <furkansenkal@hotmail.com>

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native-vector-icons|react-native|@react-native|@react-navigation)/)"
+  ],
   preset: 'react-native',
+  
 };
