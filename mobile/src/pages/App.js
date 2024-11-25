d5a31703dcfaa42c422f6e655ce435996229f4d9
furkansenkal <72168386+furkansenkal@users.noreply.github.com>
2024-11-25 20:02:30 +0300

    feat: add authorization context (login&logout logic)

--- mobile/src/pages/App.js
+++ mobile/src/pages/App.js
@@ -5,85 +5,87 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import Login from './Login';
 import Register from './Register';
 import ForgotPassword from './ForgotPassword';
-import Home from './Home'
+import Home from './Home';
 import Profile from './Profile';
 import News from './News';
 
 const Stack = createStackNavigator();
-const Sidebar = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
+// Custom Header
+const CustomHeader = ({ navigation }) => {
+  const { user } = useAuth();
 
-const navigateProfile = (navigation, username) => {
-  navigation.navigate("Profile", {username: username});
-};
-const CustomHeader = ({ navigation }) => (
-  <View style={styles.customHeader}>
-    <TouchableOpacity onPress={() => navigation.toggleDrawer()} style={styles.headerButton}>
-      <MaterialIcons name="menu" size={30} color="white" />
-    </TouchableOpacity>
-    <Text style={styles.logoText}>Bull&Bear</Text>
-    <TouchableOpacity onPress={() => navigateProfile(navigation)} style={styles.headerButton}>
-      <MaterialIcons name="account-circle" size={30} color="white" />
-    </TouchableOpacity>
-  </View>
-);
-
-
-const App = () => {
-    const LoginRelated = () => {
-      return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={Login} ></Stack.Screen>
-          <Stack.Screen name="Register" component={Register} ></Stack.Screen>
-          <Stack.Screen name="ForgotPassword" component={ForgotPassword} ></Stack.Screen>
-        </Stack.Navigator>
-      )
+  const handleProfileNavigation = () => {
+    if (user) {
+      navigation.navigate('Profile', { username: user.username });
+    } else {
+      navigation.navigate('Login&Register');
     }
+  };
 
-    /* const TabBar = (params) =>{ // This structure can be used after some point
-      const username = params.route;
-      
-      return(
-      
-      )
-    } */
+  return (
+    <View style={styles.customHeader}>
+      <TouchableOpacity onPress={() => navigation.toggleDrawer()} style={styles.headerButton}>
+        <MaterialIcons name="menu" size={30} color="white" />
+      </TouchableOpacity>
+      <Text style={styles.logoText}>Bull&Bear</Text>
+      <TouchableOpacity onPress={handleProfileNavigation} style={styles.headerButton}>
+        <MaterialIcons name="account-circle" size={30} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-    return (
-        <NavigationContainer>
-          <Sidebar.Navigator
-        screenOptions={{
-          headerShown: true, // Enable the custom header globally
-          header: ({ navigation }) => <CustomHeader navigation={navigation} />, // Use CustomHeader for all screens
-        }}
+// Login & Register Stack Navigator
+const LoginStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Register" component={Register} />
+    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+  </Stack.Navigator>
+);
+const DrawerNavigator = () => {
+  
+  const { user } = useAuth();
+  
+  return (
+  <Drawer.Navigator
+          screenOptions={{
+            headerShown: true,
+            header: ({ navigation }) => <CustomHeader navigation={navigation} />,
+          }}
         >
-            <Sidebar.Screen 
-              name="Home" 
-              component={Home}              
-            />
-            <Sidebar.Screen 
-              name="Profile" 
-              component={Profile}               
-            />
-            <Sidebar.Screen 
-              name="Login&Register" 
-              component={LoginRelated} 
-            />
-            <Sidebar.Screen 
-              name="News" 
-              component={News} 
-            />
-          </Sidebar.Navigator>
-        </NavigationContainer>
-      );
+          <Drawer.Screen name="Home" component={Home} />
+          { user  ? (
+            <Drawer.Screen name="Profile" component={Profile} />
+          ) : (
+            <Drawer.Screen name="Login&Register" component={LoginStack} />
+          )}
+          <Drawer.Screen name="News" component={News} />
+        </Drawer.Navigator>
+  );
+};
+const App = () => {
+   // Access authentication state
+
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <DrawerNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
 };
 
 const styles = StyleSheet.create({
   customHeader: {
     height: 60,
-    backgroundColor: '#0077B6', // Set to a solid color to make it fully visible
+    backgroundColor: '#0077B6',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
