f2d3653097ff39f3da023c5a34628f030ec58b1e
cemgungor1 <ccemgungor@gmail.com>
2024-11-25 18:36:21 +0300

    feat: command to add supported currencies to db at each build

---
+++ backend/marketfeed/management/commands/update_currencies.py
@@ -0,0 +1,36 @@
+import json
+from django.core.management.base import BaseCommand
+from marketfeed.models import Currency
+from django.conf import settings
+import os
+
+class Command(BaseCommand):
+    help = 'Update or Insert the supported currencies to db'
+
+    def handle(self, *args, **kwargs):
+        # Define the file path (assuming it's inside a 'currency_data' folder)
+        file_path = os.path.join(settings.BASE_DIR,'marketfeed', 'management', 'currencies.json')
+
+        # Check if the file exists
+        if not os.path.exists(file_path):
+            self.stdout.write(self.style.ERROR(f'Currency data file not found: {file_path}'))
+            return
+
+        with open(file_path, 'r') as file:
+            currencies_data = json.load(file)
+
+        for currency_data in currencies_data:
+            code = currency_data.get('code')
+            name = currency_data.get('name')
+
+            # Get or create the currency
+            currency, created = Currency.objects.get_or_create(
+                code=code,
+                defaults={'name': name}
+            )
+
+            # Output the result
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Inserted {currency.code} into the database.'))
+            else:
+                self.stdout.write(self.style.SUCCESS(f'{currency.code} already exists in the database.'))
