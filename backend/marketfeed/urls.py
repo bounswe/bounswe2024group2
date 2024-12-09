2ac1a7949bbd528d709ce76f4674fd63a06f3514
Rukiye Aslan <rukiye.aslan@getmidas.com>
2024-12-09 23:57:07 +0300

    feat: Add portfolio stock urls

--- backend/marketfeed/urls.py
+++ backend/marketfeed/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CurrencyViewSet, StockViewSet, TagViewSet, PortfolioViewSet, PostViewSet, CommentViewSet
+from .views import CurrencyViewSet, StockViewSet, TagViewSet, PortfolioViewSet, PostViewSet, CommentViewSet, PortfolioStockViewSet
 
 router = DefaultRouter()
 router.register(r'currencies', CurrencyViewSet)
@@ -9,6 +9,7 @@ router.register(r'tags', TagViewSet)
 router.register(r'portfolios', PortfolioViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
+router.register(r'portfolio-stocks', PortfolioStockViewSet, basename='portfolio-stocks')
 
 urlpatterns = [
     path('', include(router.urls)),
