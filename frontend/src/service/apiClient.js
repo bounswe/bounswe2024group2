f3c60b94bea62992571aff710f2f7ee8c0cdb47b
halil <halilibrahimkasapoglu@gmail.com>
2024-12-12 23:07:33 +0300

    fix: add logs

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -5,6 +5,10 @@ const apiClient = axios.create({
   timeout: 10000,
 });
 
+console.log(process.env)
+console.log(process.env.REACT_APP_API_BASE_URL);
+console.log(apiClient);
+
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
   if (token) config.headers.Authorization = `Bearer ${token}`;
