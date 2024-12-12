2b8cb87579a9bed3ef0faea07af14f3897c59de9
halil <halilibrahimkasapoglu@gmail.com>
2024-12-12 23:10:02 +0300

    fix: increase timeout

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000",
-  timeout: 10000,
+  timeout: 120000, 
 });
 
 console.log(process.env)
