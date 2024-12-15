25dfcb1811185fdbfed2de62ea37f50849e3ca61
oguzpancuk <44545164+oguzpancuk@users.noreply.github.com>
2024-12-15 13:03:40 +0300

    Added feature of get profile by user id
    Implemented a get method to get profiles by user id

--- backend/onboarding/views.py
+++ backend/onboarding/views.py
@@ -204,6 +204,16 @@ class ProfileViewSet(viewsets.ModelViewSet):
         currency.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(detail=False, methods=['get'], url_path='by-user-id/(?P<user_id>[^/.]+)')
+    def get_profile_by_user_id(self, request, user_id=None):
+        try:
+            profile = self.get_queryset().get(user__id=user_id)
+        except Profile.DoesNotExist:
+            return Response({'error': 'Profile not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
+
 User = get_user_model()
 
 class FollowView(generics.GenericAPIView):
